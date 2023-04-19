3c6625cd1ef117c306ee5ccf295bcc5acf4803af
OlesyaChalykh <124625557+OlesyaChalykh@users.noreply.github.com>
2023-04-19 18:08:27 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,61 +1,43 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-import time
 
 
-class TestShowMyPets:
+@pytest.fixture(autouse=True)
+def testing():
+    pytest.driver = webdriver.Chrome()
+    # Переходим на страницу авторизации
+    pytest.driver.get('http://petfriends.skillfactory.ru/login')
 
-    def setup(self):
-        self.user = "alexd84@mail.ru"
-        self.password = "Ceckbr444"
-        self.open()
+    yield
 
-
-    def open(self):
-        self.driver = webdriver.Chrome('D:\Skillfactory\PythonTests\Drivers\chromedriver.exe')
-        self.driver.get("https://petfriends.skillfactory.ru/login")
-        time.sleep(2)
-
-    def close(self):
-        self.driver.quit()
-
-    def teardown(self):
-        self.close()
-
-    def login(self):
-        self.driver.find_element(By.XPATH, "//input[@id='email']" ).send_keys(self.user)
-        self.driver.find_element(By.XPATH, "//input[@id='pass']").send_keys(self.password)
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
-        assert self.driver.find_element(By.XPATH, "//button[contains(text(),'Выйти')]")
-        time.sleep(2)
-
-    def test_show_my_pets(self):
-        self.driver.find_element(By.ID, "email").send_keys(self.user)
-        self.driver.find_element(By.ID, "pass").send_keys(self.password)
-        time.sleep(2)
-        self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
-        time.sleep(5)
-        assert self.driver.find_element(By.CSS_SELECTOR, "h1").text == "PetFriends"
-        # неявное ожидание
-        self.driver.implicitly_wait(10)
-        # явное ожидание
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Мои питомцы')]")))
-        # или
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Выйти')]")))
+    pytest.driver.quit()
 
 
-        images = self.driver.find_elements(By.CSS_SELECTOR, ('.card-deck .card-img-top'))
-        names = self.driver.find_elements(By.CSS_SELECTOR, ('.card-deck .card-title'))
-        descriptions = self.driver.find_elements(By.CSS_SELECTOR, ('.card-deck .card-text'))
+def test_show_my_pets():
+    # Вводим email
+    pytest.driver.implicitly_wait(5)
+    pytest.driver.find_element(By.ID, 'email').send_keys('test2023@mail.ru')
+    # Вводим пароль
+    pytest.driver.find_element(By.ID,'pass').send_keys('123456789')
+    # Нажимаем на кнопку входа в аккаунт
+    pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert pytest.driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"
 
-        for i in range(len(names)):
-            assert images[i].get_attribute('src') != ' '
-            assert names[i].text != ' '
-            assert descriptions[i].text != ' '
-            assert ', ' in descriptions[i].text
-            parts = descriptions[i].text.split(", ")
-            assert len(parts[0]) > 0
-            assert len(parts[1]) > 0
+
+    images = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card-deck .card-img-top')))
+    names = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card-deck .card-title')))
+    descriptions = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card-deck .card-text')))
+
+
+    for i in range(len(names)):
+        assert images[i].get_attribute('src') != '', 'Image not found'
+        assert names[i].text != '', 'Name not found'
+        assert descriptions[i].text != '', 'Description not found'
+        assert ', ' in descriptions[i]
+        parts = descriptions[i].text.split(", ")
+        assert len(parts[0]) > 0
+        assert len(parts[1]) > 0
